395607eea07eaae7839da3ed2a6c56a70b18cfdd
aschlegel <aschlegel09@gmail.com>
2018-10-31 07:18:48 -0400

    adding api routes

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(function(req, res){
-    res.send(404);
-});
+// app.use(function(req, res){
+//     res.send("404");
+// });
+// require("./app/data/friends.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
   
-  
-// var Friends = require("./app/data/friends.js");
+
 // console.log(Friends);
 
-// var api = require("./app/routing/apiRoutes.js")(app);
-// var html = require("./app/routing/htmlRoutes.js")(app);
 // console.log(api + html);
