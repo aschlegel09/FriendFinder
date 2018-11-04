cce329fd6482150a94ea8034f38af53184fd42df
aschlegel <aschlegel09@gmail.com>
2018-11-04 00:07:08 -0400

    successfully add friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,25 @@
 var friendsData = [
   {
-  name: "Andy",
-  imageUrl: "https://avatars3.githubusercontent.com/u/28871636?s=400&u=591c758dba923b7dc96247dca87aea59c11c4dcd&v=4",
-  scores: ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
-}
+    name: "Andy",
+    imageUrl: "google.com",
+    scores: ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+  },
+  {
+    name: "Charlie",
+    imageUrl: "facebook.com",
+    scores: ["3", "4", "5", "3", "3", "4", "3", "4", "2", "3"]
+  },
+  {
+    name: "Betty",
+    imageUrl: "myspace.com",
+    scores: ["4", "3", "4", "2", "4", "5", "3", "2", "3", "4"]
+  },
+  {
+    name: "Susan",
+    imageUrl: "twitter.com",
+    scores: ["4", "3", "2", "2", "4", "4", "3", "3", "4", "3"]
+  }
 ];
+
 // // Exporting our Friend array. We will require it in server.js
 module.exports = friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,4 @@
 var friendsData = require("../data/friends");
-// var bodyParser = require("body-parser");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
@@ -16,25 +15,25 @@ module.exports = function(app) {
     var inputData = req.body;
     var inputScore = inputData.scores;
 
-    var tDifferene;
+    var tDifference;
 
     for (var i = 0; i < friendsData.length; i++) {
       var thisFriend = friendsData[i];
-      tDifferene = 0;
+      tDifference = 0;
 
       console.log(thisFriend.name);
 
       for (var x = 0; x < thisFriend.scores.length; x++) {
         var thisFriendScore = thisFriend.scores[x];
-        var userScore = inputScore;
-
-        tDifferene += Math.abs(parseInt(userScore) - parseInt(thisFriendScore));
+        var userScore = inputScore[x];
+        console.log(userScore);
+        tDifference += Math.abs(parseInt(userScore) - parseInt(thisFriendScore));
       }
 
-      if (tDifferene <= friendMatch.scoreDifference) {
+      if (tDifference <= friendMatch.scoreDifference) {
         friendMatch.name = thisFriend.name;
         friendMatch.imageUrl = thisFriend.imageUrl;
-        friendMatch.scoreDifference = tDifferene;
+        friendMatch.scoreDifference = tDifference;
       }
     }
 
@@ -43,54 +42,3 @@ module.exports = function(app) {
     res.json(friendMatch);
   });
 };
-
-    
-//     if ((friendsData = true)) {
-//       var json = res.json(friendsData);
-//       console.log(req.body);
-//       console.log("Match made");
-//       console.log(json);
-//       // res.json(true);
-//     } else {
-//       alert("There are no friends to match with.");
-//       console.log("error");
-//     }
-//   });
-
-//   //option to clear
-//   app.post("/api/clear", function(req, res) {
-//     friendsData.length = [];
-//     res.json({ ok: true });
-//   });
-// };
-
-// app.get("/api/friends", function(req, res) {
-//   return res.json(friendsData);
-// });
-
-// app.get("/api/friends/:friend", function(req, res) {
-//   var chosen = req.params.friendsData;
-//   res.sendFile(path.join(__dirname, "../public/survey.html"));
-//   console.log(chosen);
-
-//   for (var i = 0; i < friendsData.length; i++) {
-//     if (chosen === friendsData[i].routeName) {
-//       return res.json(friendsData[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
-
-// app.post("/api/friends", function(req, res) {
-//   var newFriend = req.body;
-
-//   newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-
-//   console.log(newFriend);
-
-//   friendsData.push(newFriend);
-
-//   res.json(newFriend);
-//   console.log(friendsData);
-// });
