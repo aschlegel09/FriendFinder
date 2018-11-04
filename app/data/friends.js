bd7227bd25d101c13e15f0cb99529da59873709f
aschlegel <aschlegel09@gmail.com>
2018-11-03 20:37:18 -0400

    friends file update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,31 +1,9 @@
-var friendsData = [{
+var friendsData = [
+  {
   name: "Andy",
   imageUrl: "https://avatars3.githubusercontent.com/u/28871636?s=400&u=591c758dba923b7dc96247dca87aea59c11c4dcd&v=4",
-  scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+  scores: ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
 }
 ];
 // // Exporting our Friend array. We will require it in server.js
 module.exports = friendsData;
-
-// // Constructor function for creating friends objects
-// var Friends = function(name, image) {
-//   this.name = name;
-//   this.imageUrl = imageUrl;
-//   this.scores = scores;
-
-//   // Prints out slightly different i0nformation
-//   this.readProfile = function() {
-//       console.log(this.name + this.imageUrl + this.scores);
-//   };
-//   this.addFriend = function(name, imageUrl, scores) {
-//     this.name.push(new Friends(name, imageUrl, scores));
-//   };
-// };
-
-// // CRUD HERE
-//req.body.name
-//req.body.imageUrl
-//req.body.scores
-
-//req.body  
-
